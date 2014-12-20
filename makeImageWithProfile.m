3d0cc9a290be1bb1a4ecc27c7d804f55af0c17bd
Tomasz Roszczynialski <roszcz@gmail.com>
2014-12-20 16:20:22 +0100

    Commitial init

---
+++ makeImageWithProfile.m
@@ -0,0 +1,35 @@
+function [out] = makeImageWithProfile(PARENT,IM,x,y,cutPoint)
+% function prepares VBoxFlex element with 2D image and 
+% its cross section at selected point
+
+theGrid = uiextras.VBoxFlex('Parent',PARENT,'Spacing',5);
+
+% make 2D image
+box1 = uiextras.BoxPanel('Parent',theGrid,...
+                         'Title', 'Lens',...
+                         'Padding',5);
+axImage = axes('Parent',box1,...
+               'ActivePositionProperty',...
+               'OuterPosition' );
+           
+imagesc(x,y,IM,'Parent',axImage);  
+colormap gray
+
+% make cross section
+box2 = uiextras.BoxPanel('Parent',theGrid,...
+                         'Title', 'Lens cross section',...
+                         'Padding',5);
+
+axPlot = axes('Parent',box2,...
+               'ActivePositionProperty',...
+               'OuterPosition' );
+           
+plot(axPlot,x,IM(cutPoint,:));
+
+% must be set after creation of cildren
+set(theGrid, 'Sizes', [-5 -2] );
+
+
+out = theGrid;
+
+end
